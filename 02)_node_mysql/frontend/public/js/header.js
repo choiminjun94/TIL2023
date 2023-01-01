400993f3337b45b7349bbde500b11f35c6cd929d
choiminjun94 <1020alswns@gmail.com>
2023-01-01 22:17:30 +0900

    Header & index.css and header.js and html file add

---
+++ 02)_node_mysql/frontend/public/js/header.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    document.getElementById("header").innerHTML
+    ='<object type="text/html" data="../view/header.html" style="width:100%; height:100%;"></object>';
+    document.getElementById("header").style.height="50px"
+})
