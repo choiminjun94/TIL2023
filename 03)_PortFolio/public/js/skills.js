57d6282c6ade92892ca9f794ccbbca34373f2b71
choiminjun94 <1020alswns@gmail.com>
2023-01-07 17:30:40 +0900

    skills 작업중

---
+++ 03)_PortFolio/public/js/skills.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    document.getElementById("skills").innerHTML
+    ='<object type="text/html" data="./view/skills.html" style="height:1102px; width:100%;"></object>';
+    document.getElementById("skills").style.height="1102px"
+})
