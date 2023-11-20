684f77111e00bbffefe6ec8911c68b8ad73e8cad
choiminjun94 <1020alswns@gmail.com>
2023-11-20 13:50:30 +0900

    mysql 복습 1일차

--- 99)_Mysql/index.sql
+++ 99)_Mysql/index.sql
@@ -1,54 +1,45 @@
--- 현재 서버에 어떤 DB가 있는지 보기 
-show DATABASES;
+-- Day1
 
--- 사용할 데이터베이스 지정 
+show databases;
+
+-- 사용할 디비 지정 (use)
 use world;
 
--- 데이터베이스 world의 데이블 이름 보기  
+-- 데이터베이터 테이블 이름 보기 (show)
 show tables;
 
--- 데이터베이스의 데이블 정보 조회 
+-- 테이블 정보 조회 (status)
 show table status;
 
--- 무슨 열이 있는지 확인 //속성 정보 
+-- 특정 테이블의 무슨 열이 있는지 확인 (테이블 속성 정보)
 desc city;
 
---  country 테이블과 countrylanguage 정보보기 
 desc country; 
-
 desc countrylanguage;
 
-
--- 요구하는 데이터를 가져오는 구분 SELECT 
-show databases;
-use world;
-
+-- 요구하는 데이터를 가져오는 구문 (select)
 SELECT * from city;
 
--- 데이블에서 필요로 하는 열만 가져오기 가능
--- 여러 개의 열을 가져오고 싶을 때는 콤마로 구분 
+-- 테이블에서 필요로 하는 열만 가져오기 
+-- 여러 개의 열을 가져오고 싶을 때는 ,로 구분 
 
-select name, district from city;
+SELECT name, district from city;
 
--- 조회하는 결과에 특정한 조건 줘서 원하는 데이터만 보고 싶을 떄 사용
--- select 필터 이름 from 테이블 이름 where 조건식; 
- 
-SELECT * from city WHERE population > 800000
+-- 조회하는 결과에 특정한 조건 줘서 원하는 데이터만 보기 
 
--- 관계 연산자의 사용 
-select * from city WHERE Population > 7000000 and Population < 8000000;
+SELECT * from city where population > 800000;  
+
+-- 과제
+-- 한국 인구만
+SELECT * from city where countrycode = 'kor';
+
+-- 미국 인구만
+SELECT * from city where countrycode = 'usa'; 
+
+-- 한국 인구중 1,000,000만 
+SELECT * from city where countrycode = 'kor' and  population >=1000000;
 
 
--- 과제 
-
--- 한국에 있는 도시만 보기
-SELECT * FROM city WHERE CountryCode = 'kor';
-
--- 미국에 있는 도시만 보기
-SELECT  * from city where CountryCode ='usa';
-
--- 한국 도시중 인수가 1,000,000 이상 인 도시만 보기 
-SELECT * from city  WHERE  CountryCode ='kor' and  Population > 1000000;
 
 -- 데이터가 숫자로 구성도어 있어 연속적인 값은 Between ... AND 사용 가능
 
