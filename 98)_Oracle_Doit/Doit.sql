7ad69742d235b9c8bfba16afb14d358fcdd24fe7
choiminjun94 <1020alswns@gmail.com>
2023-11-21 02:02:54 +0900

    Oracle 기본서와 문제집 파일 분리

--- 98)_Oracle_Doit/Doit.sql
+++ 98)_Oracle_Doit/Doit.sql
@@ -1,8 +1,6 @@
-# Oracle 쿼리 정리
+select * from all_users;
 
-## DAY1
-
-### emp 테이블이 없어서 생성 
+-- emp 테이블이 없어서 생성 
 CREATE TABLE EMP (
  EMPNO               NUMBER(4) NOT NULL,
  ENAME               VARCHAR2(10),
@@ -31,79 +29,52 @@ INSERT INTO EMP VALUES (7934,'MILLER','CLERK',7782,TO_DATE('1982-01-11','YYYY-MM
 
 COMMIT;
 
-## Day.1 (DO it)
-
-### 데이터 출력  테이블 전체 열 출력 
+-- Day.1 (DO it)
+-- 데이터 출력
+-- 테이블 전체 열 출력 
 SELECT * FROM emp;
 
-### 테이블 부분열만 출력 (사원번호, 이름, 부서)
+-- 테이블 부분열만 출력 (사원번호, 이름, 부서)
 SELECT empno, ename, job FROM emp;
 
-### 복습 (사원번호 부서번호 출력)
+-- 복습 (사원번호 부서번호 출력)
 SELECT empno, deptno FROM emp; 
 
-### 열 제거
-### 중복 열 제거 (DISTINCT)
+-- 열 제거
+-- 중복 열 제거 (DISTINCT)
 SELECT  DISTINCT deptno FROM emp; 
 
-### 여러 중복열 제거
+-- 여러 중복열 제거
 SELECT DISTINCT job, deptno FROM emp;
 
-### 별칭 지정
-### 별칭 (열과 연산식 )
+-- 별칭 지정
+-- 별칭 (열과 연산식 )
 SELECT ename, sal, sal*12+Comm,comm FROM emp;
 
-### 별칭 지정2 (as)
+-- 별칭 지정2 (as)
 SELECT ename AS 이름, sal AS 월급, sal*12+comm AS 연간총수입, comm FROM emp; 
 
-### 원하는 순서로 출력 데이터를 정렬 (order by)
-### 오름차순
+-- 원하는 순서로 출력 데이터를 정렬 (order by)
+-- 오름차순
 SELECT * FROM emp ORDER BY sal;
 
-### 복습 (사원번호 오름차순)
+-- 복습 (사원번호 오름차순)
  SELECT * FROM emp ORDER BY empno;
  
-### 급여 내림차순 (desc)
+-- 급여 내림차순 (desc)
 SELECT * FROM emp ORDER BY sal DESC; 
 
-### 오름차순과 내림차순 동시에 사용
+-- 오름차순과 내림차순 동시에 사용
 SELECT * 
 	FROM emp 
 ORDER BY DEPTNO, sal DESC;
 
-### 과제 (잡 중복 제거하기)
+-- 과제 (잡 중복 제거하기)
 SELECT DISTINCT  job AS 직업 FROM emp; 
 
 SELECT empno AS "EMPLOYEE_NO", ename AS "EMPLOYEE_NAME", job, MGR  AS "MANAGER", hiredate ,sal AS "SALARY", comm AS "COMMISSION", deptno AS "DEPARTMENT_NO" 
 
 	FROM emp ORDER BY EMPNO DESC, ENAME ;
-	
 
-## Day.1 (200제)
-
-### 001) 사원번호 및 이름, 월급만 
-SELECT empno, ename,sal FROM emp; 
-
-### 002) 테이블에서 모든 열 출력
-SELECT * FROM emp;
-
-### 003) 컬럼 별칭을 사용하여 출력되는 컬럼명 변경
-SELECT empno AS "사원번호", ename AS "이름", sal FROM emp;
-
-### 004) 사원테이블의 이름과 월급을 붙여서 출력 (몰랐음)
-SELECT ename || sal FROM emp;
-
-SELECT ename || '의 월급은 ' || sal ||' 입니다.' AS  월급정보  FROM emp;
-
-### 005)사원 직업에서 중복된 데이터 제거 (DISTINCT)
-SELECT DISTINCT job FROM emp; 
-
-### 006) 이름과 월급을 출력하는데 월급이 낮은 사원별로 출력 (asc)
-SELECT ename, sal FROM emp ORDER BY sal ASC;
-
-### 006.1) 이름과 부서번호는 내림차순 월급은 올림차순으로 출력
-SELECT ename AS "이름", DEPTNO  AS "부서번호", sal AS "월급" 
-	FROM emp 
-ORDER BY DEPTNO asc , sal desc; 
 
 
